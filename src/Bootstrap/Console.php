7e80f318e9a205bbd7b8a9e559ff71a3328d70cc
dengrunfang <18024158375@163.com>
2020-11-02 14:41:15 +0800

    sss

---
+++ src/Bootstrap/Console.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description 控制台启动类
+ * Created by PhpStorm
+ * Author: runfang.deng
+ * Date:   2020/11/2 12:10
+ */
+
+namespace Framework\Bootstrap;
+
+use Framework\Console\App;
+
+class Console
+{
+    public function boot(): void
+    {
+        (new App())->start(); //启动应用 初始化容器 注册核心服务
+    }
+}
---
+++ src/Console/App.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description App
+ * Created by PhpStorm
+ * Author: runfang.deng
+ * Date:   2020/11/2 12:13
+ */
+
+
+namespace Framework\Console;
+
+
+class App
+{
+    public function start(){
+        global $server_argv;
+        print_r($server_argv);
+    }
+}
